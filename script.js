21884802c7c818efc25682e91c56ff47fb3740f3
Manon <31137629+web-Nuo@users.noreply.github.com>
2020-05-30 16:12:35 +0200

    Calculator now also accepts commas instead of dots

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 document.getElementById("container").onchange = function () {
   // grab user input
-  let bill = Number(document.getElementById("billTotal").value);
+  let bill = document.getElementById("billTotal").value;
+  // convert string to number and replace commas by dots
+  bill = Number(bill.replace(",", "."));
+
   let tipPercent = document.getElementById("tipInput").value;
   let split = document.getElementById("splitInput").value;
   // assign input to output IDs so they can appear on screen
@@ -10,6 +13,7 @@ document.getElementById("container").onchange = function () {
   let tipValue = bill * (tipPercent / 100);
   let newBillEach = (bill + tipValue) / split;
   let tipEach = tipValue / split;
+  // assign input to output IDs so the values appear on screen
   document.getElementById("newBill").innerHTML = newBillEach.toFixed(2) + " €";
   document.getElementById("tipPerP").innerHTML = tipEach.toFixed(2) + " €";
   document.getElementById("people").innerHTML =
